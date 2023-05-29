ce39948378b2ca26fcce20a3d5f797462974b18a
MinhTran <nhuminh24122000@gmail.com>
2023-05-29 22:12:01 +0700

    First code

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { lazy } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HomeTemplate from './templates/HomeTemplate/HomeTemplate'
+
+const Home = lazy(() => import('./pages/Home/Home'));
+const Login = lazy(() => import('./pages/Login/Login'));
+const Register = lazy(() => import('./pages/Register/Register'));
+const Detail = lazy(() => import('./pages/Detail/Detail'));
+const Carts = lazy(() => import('./pages/Carts/Carts'));
+const Search = lazy(() => import('./pages/Search/Search'));
+const Profile = lazy(() => import('./pages/Profile/Profile'));
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='' element={<HomeTemplate />}>
+                    <Route index element={<Home />} />
+                    <Route path='login' element={<Login />} />
+                    <Route path='register' element={<Register />} />
+                    <Route path='carts' element={<Carts />} />
+                    <Route path='detail'>
+                        <Route path=':productID' element={<Detail />} />
+                    </Route>
+                    <Route path='search' element={<Search />} />
+                    <Route path='profile' element={<Profile />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default App
---
+++ src/components/CardBody/CardBody.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CardBody() {
+  return (
+    <div>CardBody</div>
+  )
+}
+
+export default CardBody
